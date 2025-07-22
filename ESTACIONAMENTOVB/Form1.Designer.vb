c1809545a4bbb2bcaeab5f4a1d17b3e940aa4c9a
Isacthomaz27 <isac.thomaz27@gmail.com>
2025-07-22 09:50:31 -0300

    Primeiro commit do projeto de estacionamento

---
+++ ESTACIONAMENTOVB/Form1.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.dgvVagas = New System.Windows.Forms.DataGridView()
+        Me.cbAndar = New System.Windows.Forms.ComboBox()
+        Me.cbVaga = New System.Windows.Forms.ComboBox()
+        Me.btnOcupar = New System.Windows.Forms.Button()
+        Me.btnLiberar = New System.Windows.Forms.Button()
+        Me.btnSalvar = New System.Windows.Forms.Button()
+        Me.btnCarregar = New System.Windows.Forms.Button()
+        CType(Me.dgvVagas, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft YaHei UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(236, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(349, 28)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Gerenciador de Estacionamento"
+        '
+        'dgvVagas
+        '
+        Me.dgvVagas.AllowDrop = True
+        Me.dgvVagas.AllowUserToOrderColumns = True
+        Me.dgvVagas.ColumnHeadersHeight = 30
+        Me.dgvVagas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.dgvVagas.DefaultCellStyle = DataGridViewCellStyle1
+        Me.dgvVagas.GridColor = System.Drawing.SystemColors.GrayText
+        Me.dgvVagas.Location = New System.Drawing.Point(12, 86)
+        Me.dgvVagas.Name = "dgvVagas"
+        Me.dgvVagas.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal
+        Me.dgvVagas.Size = New System.Drawing.Size(752, 107)
+        Me.dgvVagas.TabIndex = 1
+        '
+        'cbAndar
+        '
+        Me.cbAndar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cbAndar.FormattingEnabled = True
+        Me.cbAndar.Location = New System.Drawing.Point(106, 225)
+        Me.cbAndar.Name = "cbAndar"
+        Me.cbAndar.Size = New System.Drawing.Size(167, 28)
+        Me.cbAndar.TabIndex = 2
+        '
+        'cbVaga
+        '
+        Me.cbVaga.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cbVaga.FormattingEnabled = True
+        Me.cbVaga.Location = New System.Drawing.Point(106, 274)
+        Me.cbVaga.Name = "cbVaga"
+        Me.cbVaga.Size = New System.Drawing.Size(167, 28)
+        Me.cbVaga.TabIndex = 3
+        '
+        'btnOcupar
+        '
+        Me.btnOcupar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnOcupar.Location = New System.Drawing.Point(313, 217)
+        Me.btnOcupar.Name = "btnOcupar"
+        Me.btnOcupar.Size = New System.Drawing.Size(192, 37)
+        Me.btnOcupar.TabIndex = 4
+        Me.btnOcupar.Text = "Registrar Ocupada"
+        Me.btnOcupar.UseVisualStyleBackColor = True
+        '
+        'btnLiberar
+        '
+        Me.btnLiberar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLiberar.Location = New System.Drawing.Point(313, 267)
+        Me.btnLiberar.Name = "btnLiberar"
+        Me.btnLiberar.Size = New System.Drawing.Size(192, 37)
+        Me.btnLiberar.TabIndex = 5
+        Me.btnLiberar.Text = "Registrar Liberada"
+        Me.btnLiberar.UseVisualStyleBackColor = True
+        '
+        'btnSalvar
+        '
+        Me.btnSalvar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSalvar.Location = New System.Drawing.Point(515, 219)
+        Me.btnSalvar.Name = "btnSalvar"
+        Me.btnSalvar.Size = New System.Drawing.Size(120, 37)
+        Me.btnSalvar.TabIndex = 6
+        Me.btnSalvar.Text = "Salvar"
+        Me.btnSalvar.UseVisualStyleBackColor = True
+        '
+        'btnCarregar
+        '
+        Me.btnCarregar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCarregar.Location = New System.Drawing.Point(515, 269)
+        Me.btnCarregar.Name = "btnCarregar"
+        Me.btnCarregar.Size = New System.Drawing.Size(120, 37)
+        Me.btnCarregar.TabIndex = 7
+        Me.btnCarregar.Text = "Carregar"
+        Me.btnCarregar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(793, 326)
+        Me.Controls.Add(Me.btnCarregar)
+        Me.Controls.Add(Me.btnSalvar)
+        Me.Controls.Add(Me.btnLiberar)
+        Me.Controls.Add(Me.btnOcupar)
+        Me.Controls.Add(Me.cbVaga)
+        Me.Controls.Add(Me.cbAndar)
+        Me.Controls.Add(Me.dgvVagas)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgvVagas, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents cbAndar As ComboBox
+    Friend WithEvents cbVaga As ComboBox
+    Friend WithEvents btnOcupar As Button
+    Friend WithEvents btnLiberar As Button
+    Friend WithEvents btnSalvar As Button
+    Friend WithEvents btnCarregar As Button
+    Friend WithEvents dgvVagas As DataGridView
+End Class
---
+++ ESTACIONAMENTOVB/Form1.vb
@@ -0,0 +1,105 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Dim vagas(1, 9) As Boolean ' 2 andares, 10 vagas
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For i As Integer = 1 To vagas.GetLength(0)
+            cbAndar.Items.Add("Andar " & i)
+        Next
+
+        For i As Integer = 1 To vagas.GetLength(1)
+            cbVaga.Items.Add("Vaga " & i)
+        Next
+
+        cbAndar.SelectedIndex = 0
+        cbVaga.SelectedIndex = 0
+
+        AtualizarGrid()
+    End Sub
+
+    Sub AtualizarGrid()
+        dgvVagas.Columns.Clear()
+        dgvVagas.Rows.Clear()
+
+        For j As Integer = 0 To vagas.GetLength(1) - 1
+            dgvVagas.Columns.Add("V" & (j + 1), "Vaga " & (j + 1))
+        Next
+
+        For i As Integer = 0 To vagas.GetLength(0) - 1
+            Dim row(vagas.GetLength(1) - 1) As String
+            For j As Integer = 0 To vagas.GetLength(1) - 1
+                row(j) = If(vagas(i, j), "Ocupada", "Livre")
+            Next
+            dgvVagas.Rows.Add(row)
+            dgvVagas.Rows(i).HeaderCell.Value = "Andar " & (i + 1)
+        Next
+
+        dgvVagas.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells
+        dgvVagas.RowHeadersWidth = 85
+        dgvVagas.BorderStyle = BorderStyle.None
+        dgvVagas.EnableHeadersVisualStyles = False
+        dgvVagas.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells
+        dgvVagas.ScrollBars = ScrollBars.Both
+
+    End Sub
+
+    Private Sub btnOcupar_Click(sender As Object, e As EventArgs) Handles btnOcupar.Click
+        Dim andar As Integer = cbAndar.SelectedIndex
+        Dim vaga As Integer = cbVaga.SelectedIndex
+
+        If vagas(andar, vaga) Then
+            MessageBox.Show("Já está ocupada.")
+        Else
+            vagas(andar, vaga) = True
+            AtualizarGrid()
+        End If
+    End Sub
+
+    Private Sub btnLiberar_Click(sender As Object, e As EventArgs) Handles btnLiberar.Click
+        Dim andar As Integer = cbAndar.SelectedIndex
+        Dim vaga As Integer = cbVaga.SelectedIndex
+
+        If Not vagas(andar, vaga) Then
+            MessageBox.Show("Já está livre.")
+        Else
+            vagas(andar, vaga) = False
+            AtualizarGrid()
+        End If
+    End Sub
+
+    Private Sub btnSalvar_Click(sender As Object, e As EventArgs) Handles btnSalvar.Click
+        Dim caminho As String = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & "\vagas.txt"
+        Using sw As New StreamWriter(caminho)
+            For i As Integer = 0 To vagas.GetLength(0) - 1
+                Dim linha As String = ""
+                For j As Integer = 0 To vagas.GetLength(1) - 1
+                    linha &= If(vagas(i, j), "1", "0")
+                Next
+                sw.WriteLine(linha)
+            Next
+        End Using
+        MessageBox.Show("Arquivo salvo em: " & caminho)
+    End Sub
+
+    Private Sub btnCarregar_Click(sender As Object, e As EventArgs) Handles btnCarregar.Click
+        Dim caminho As String = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & "\vagas.txt"
+        If File.Exists(caminho) Then
+            Dim linhas() As String = File.ReadAllLines(caminho)
+            For i As Integer = 0 To linhas.Length - 1
+                For j As Integer = 0 To linhas(i).Length - 1
+                    vagas(i, j) = (linhas(i)(j) = "1")
+                Next
+            Next
+            AtualizarGrid()
+        Else
+            MessageBox.Show("Arquivo não encontrado.")
+        End If
+
+
+
+    End Sub
+
+
+
+End Class
